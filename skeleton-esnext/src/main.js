8bd4b93c28f52845940414c55a0c43b6e84a0f20
charles.pockert@googlemail.com <charles.pockert@googlemail.com>
2016-06-17 12:16:17 +0100

    test router fixes

--- skeleton-esnext/src/main.js
+++ skeleton-esnext/src/main.js
@@ -6,9 +6,10 @@ export function configure(aurelia) {
     .developmentLogging();
 
   //Uncomment the line below to enable animation.
-  //aurelia.use.plugin('aurelia-animator-css');
+  aurelia.use.plugin('aurelia-animator-css');
   //if the css animator is enabled, add swap-order="after" to all router-view elements
-
+  aurelia.use.globalResources('layout-router-view');
+  aurelia.use.globalResources('layout-router-view-broken');
   //Anyone wanting to use HTMLImports to load views, will need to install the following plugin.
   //aurelia.use.plugin('aurelia-html-import-template-loader')
 
